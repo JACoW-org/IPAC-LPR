9311ea5dfebd10213e51ffe7a23a57e5d0101e7a
delerue <delerue@lal.in2p3.fr>
2025-03-01 23:21:08 +0100

    Added the creating of the past orals page

--- map_authors_to_MC.py
+++ map_authors_to_MC.py
@@ -38,17 +38,14 @@ else:
     confids=[args.confid ]    
 
 uf.load_users()
+all_oral_speakers=[]
 
 for confid in confids:
 
     authors_map_fname=f'data/all_authors_by_subMC_for_conf_{confid}.data'
-    
-    json_contribs=jnf.load_contribs(confid)
-    #print(json_contribs.keys())
     #print(len(json_contribs["results"]))
     print("Loading contribution from conf id",confid,":", json_contribs["results"][0]["title"])
     the_sub_MC_list=[]
-    all_oral_speakers=[]
     all_authors_by_sub_MC=[]
     all_authors_by_sub_MC_speakers=[]
     all_authors_by_sub_MC_coauthors=[]
@@ -57,16 +54,26 @@ for confid in confids:
     for contrib in json_contribs["results"][0]['contributions']:
         print(".",end='')       
         sys.stdout.flush()
+        keep_contrib=True
         if (contrib['track'] is None):
             if contrib['type'] == 'Invited Oral Presentation':
                 contrib['track']='MC0.00'
+            elif contrib['type'] == 'Special (Award) Presentation' or contrib['type'] == 'Poster Presentation':
+                keep_contrib=False
+            elif contrib['type']==None:
+                #Some contributions witout type or track exist in indico...
+                #We ignore them
+                keep_contrib=False
             else:
                 print(contrib)
                 print(contrib['track'])
                 print(contrib['type'])
-                print("Contrib without track")
+                print(contrib['speakers'][0]['first_name'])
+                print(contrib['speakers'][0]['last_name'])
+                print("Contrib without track nor type")                
+                keep_contrib=False
                 exit()
-        if (1==1):
+        if keep_contrib:
             spacePos=contrib['track'][0:7].find(' ')
             if spacePos>0:
                 subTrackName=contrib['track'][0:spacePos]
@@ -103,7 +110,8 @@ for confid in confids:
                         if theperson['is_speaker']:
                             uf.add_paper_to_user(userid,confid,str(contrib['db_id']),'speakers')
                             all_authors_by_sub_MC_speakers[idx].append(userid)
-                            all_oral_speakers.append({ 'user_id': userid , 'contrib_id': str(contrib['db_id'])  , "conf_id": confid })                        
+                            if 'oral' in  the_contrib['type']['name'].lower():
+                                all_oral_speakers.append({ 'user_id': userid , 'contrib_id': str(contrib['db_id'])  , "conf_id": confid })                        
                         if theperson['author_type']=='primary':
                             auth_type='primaryauthors'
                         elif theperson['author_type']=='secondary':
@@ -125,7 +133,7 @@ for confid in confids:
 
             for auth_type in ['speakers' , 'primaryauthors' ,'coauthors' ]:
                 for speak in contrib[auth_type]:
-                    print('speak',speak)
+                    #print('speak',speak)
                     #speak_name=speak['first_name']+" "+speak['last_name']
                     #print('\tSpeak name:',speak_name)
                     if 'email_hash' in speak.keys() and speak['email_hash'] is not None and len(speak['email_hash'])>3:
@@ -136,7 +144,8 @@ for confid in confids:
                             all_authors_by_sub_MC_coauthors[idx].append(userid)
                         elif auth_type == 'speakers': 
                             all_authors_by_sub_MC_speakers[idx].append(userid)
-                            all_oral_speakers.append({ 'user_id': userid , 'contrib_id': str(contrib['db_id']) , "conf_id": confid})                        
+                            if 'oral' in  contrib['type']['name'].lower():                            
+                                all_oral_speakers.append({ 'user_id': userid , 'contrib_id': str(contrib['db_id']) , "conf_id": confid})                        
                         else:
                             all_authors_by_sub_MC[idx].append(userid)
     for subMC in the_sub_MC_list:
@@ -164,7 +173,8 @@ uf.users_purity()
 uf.save_users()
 
 if args.create_orals_page:
-    print("Creating orals page.",len(all_oral_speakers)," speakers found.")
+    print(" *** Creating orals page. ***")
+    print(len(all_oral_speakers)," speakers found.")
     orals_page_name="IPAC_past_orals.html"
     orals_page_header="sort-table_header.html"
     orals_page_footer="sort-table_footer.html"
@@ -175,6 +185,19 @@ if args.create_orals_page:
         oral_page.write(line)
     opr.close()
     #check for duplicates
+    print("Checking for duplicate speakers")
+    for ispeaker in range(len(all_oral_speakers)):
+        for jspeaker in range(0,ispeaker):
+            #print(ispeaker,jspeaker,all_oral_speakers[ispeaker]['contrib_id'],all_oral_speakers[jspeaker]['contrib_id'],all_oral_speakers[ispeaker]['user_id'],all_oral_speakers[jspeaker]['user_id'])
+            if all_oral_speakers[ispeaker]==all_oral_speakers[jspeaker]:
+                print("duplicate 1")
+                exit()
+            if all_oral_speakers[ispeaker]['contrib_id']==all_oral_speakers[jspeaker]['contrib_id']:
+                if all_oral_speakers[ispeaker]['user_id']==all_oral_speakers[jspeaker]['user_id']:
+                    print("duplicate 2")
+                    exit()
+    print("Checked for duplicate speakers")
+                    
     for speaker in all_oral_speakers:
         #print("Speaker",speaker)
         speaker_data=uf.find_user(user_id=speaker['user_id'])
@@ -185,27 +208,40 @@ if args.create_orals_page:
             exit()
         #print(speaker_data["first_name"],speaker_data["last_name"])
         the_contrib=pf.get_contrib_info(speaker['contrib_id'],event_id=confid,use_cache=True,file_age_to_renew=-1)
-        print(the_contrib)
+        #print(the_contrib)
+        if type(the_contrib["type"]) is dict:
+            contrib_type=the_contrib["type"]['name']
         if the_contrib["track"] is None:
-            the_contrib["track"]="MC0.00"
+            contrib_track="MC0.00"
+        elif type(the_contrib["track"]) is dict:
+            if len(the_contrib["track"]['code'])>2:
+                contrib_track=the_contrib["track"]['code']
+            elif len(the_contrib["track"]['title'])>2:
+                contrib_track=the_contrib["track"]["title"][0:7]
+            else:
+                print(the_contrib["track"])
+                exit()
+        contrib_url="<A HREF='https://indico.jacow.org/event/"+speaker['conf_id']+"/contributions/"+speaker['contrib_id']+"/'>"+speaker['contrib_id']+"</A>"
         speaker_found=False
         for person in the_contrib['persons']:
             #print(person['is_speaker'])
             if person['is_speaker']:
-                if speaker_found:
-                    print("Multiple speakers found")
-                speaker_found=True
-            oral_page.writeln("<tr>")
-            for txt in [ person["first_name"],person["last_name"],person["affiliation"],the_contrib["track"],the_contrib["type"],the_contrib["title"],pf.get_conf_from_id(speaker['conf_id']) ]:
-                print(txt,end='')
-                oral_page.writeln("<td>")
-                oral_page.writeln(txt)
-                oral_page.writeln("</td>")
-            oral_page.writeln("</tr>")
+                print("***", speaker['contrib_id'], person['last_name'],speaker_data['last_name'])
+                if person['last_name']==speaker_data['last_name']:
+                    if speaker_found:
+                        print("Multiple speakers found")
+                    oral_page.write("<tr>\n")                
+                    for txt in [ person["first_name"],person["last_name"],person["affiliation"],contrib_track,contrib_type,the_contrib["title"],pf.get_conf_from_id(speaker['conf_id']) , contrib_url ]:
+                        print(txt,end=' ')
+                        oral_page.write("<td>\n")
+                        oral_page.write(txt+"\n")
+                        oral_page.write("</td>\n")
+                    oral_page.write("</tr>\n")
+                    speaker_found=True
 
     opf=open(orals_page_footer,"r")
     for line in opf:
         oral_page.write(line)
     opf.close()
-                    
+    oral_page.close()
         
